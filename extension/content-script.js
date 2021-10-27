c606359a082b356c19f4b865a6cef6bbee4de4b5
cazhjo <cazpersihvonen@hotmail.com>
2021-10-27 20:20:07 +0200

    began automatic insertion based on viewcount

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,5 +1,3 @@
-import { twitchFollowItem } from './html-elements.js'
-
 const streamerInfo = {
     name: "DrDisRespect",
     avatar: "https://yt3.ggpht.com/tvkeak3Sfk6blV7sMIO6CjhUMSTmNTSgiBF2NOaj7V0xuZPEvkmARxgKtCUFpyONef-LS1ZW=s88-c-k-c0x00ffffff-no-rj",
@@ -11,10 +9,41 @@ const streamerInfo = {
 
 const customStreamers = [streamerInfo]
 
+const replaceHTMLCollection = (parent, array) => {
+    parent.innerHTML = ""
+    for (let item of array)
+        parent.appendChild(item)
+}
+
+const updateFollowList = (followList, streamers) => {
+    let top12 = [...followList.children].slice(0, 12)
+    for (let streamer of streamers) {
+        // let current = followList.getElementsByClassName(streamer.name)[0]
+        // if (current) {
+        //     current.getElementsByClassName["viewCount"].innerText = formatViewCount(streamer.viewCount)
+        //     continue
+        // }
+
+        for (let i = 0; i < top12.length; i++) {
+            let viewCount = unformatViewCount(top12[i].getElementsByClassName("Layout-sc-nxg1ff-0 faqhMI")[0].firstChild.innerText)
+            if (streamer.viewCount >= viewCount) {
+                top12.splice(i, 0, twitchFollowItem(streamer))
+            }
+        }
+    }
+
+    // if (top12.length > 12)
+    //     top12.pop()
+
+    // replaceHTMLCollection(followList, top12)
+}
+
 window.addEventListener('load', () => {
+    console.log('here')
     const followList = document.getElementsByClassName("InjectLayout-sc-588ddc-0 dBaosp tw-transition-group")[0]
-    let customItem = twitchFollowItem(streamerInfo)
-    followList.prepend(customItem)
     console.log(followList)
+    updateFollowList(followList, customStreamers)
+    // let customItem = twitchFollowItem(streamerInfo)
+    // followList.prepend(customItem)
 })
 
--- extension/utils.js
+++ extension/utils.js
@@ -1,4 +1,4 @@
-export const formatViewCount = (viewCount) => {
+const formatViewCount = (viewCount) => {
     if (viewCount >= 1000 && viewCount < 100000) {
         return (viewCount / 1000).toFixed(1) + "K" 
     } else if (viewCount >= 100000 && viewCount < 1000000) {
@@ -7,5 +7,18 @@ export const formatViewCount = (viewCount) => {
         return (viewCount / 1000000).toFixed(1) + "M"
     } else {
         return viewCount
+    }
+}
+
+const unformatViewCount = (viewCount) => {
+    let suffix = viewCount[viewCount.length-1]
+    let value = viewCount.slice(0, viewCount.length-1)
+
+    if (suffix === "K") {
+        return value * 1000 
+    } else if (suffix === "M") {
+        return value * 1000000
+    } else {
+        return +viewCount
     }
 }
